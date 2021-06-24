b7f3b119706ecbc1e1f72acf126ad6a188ed320b
200514 <78738239+200514@users.noreply.github.com>
2021-06-24 18:29:44 +0200

    Opdracht 4

--- index.php
+++ index.php
@@ -1,35 +1,32 @@
 <?php
 class macbook
 {
+    public $naam = "";
     public $kleur = "";
     public $matriaal = "";
     public $vorm = "";
     public $aanstroom = false;
   
 
-    public function __construct($kleur, $matriaal, $vorm, $aanstroom)
+    public function __construct($naam, $kleur, $matriaal, $vorm, $aanstroom)
     {
+        $this->naam = $naam;
         $this->kleur = $kleur;
         $this->matriaal = $matriaal;
         $this->vorm = $vorm;
         $this->aanstroom = $aanstroom;
     }
-    public function accu()
-    {
-      if ($this->aanstroom == true)
-      {
-        echo "De accu wordt opgeladen <br>";
+    public function intro() {
+        echo "Dit is de {$this->naam}. De kleur is {$this->kleur}. Hij is gemaakt van {$this->matriaal}, en de vorm is {$this->vorm}. 
+        <br>";
       }
-      else{
-        echo "De accu wordt niet opgeladen <br>";
+     }
+     
+     class toetsenbord extends macbook {
+       public function message() {
+         echo "hiermee kan je typen<br> ";
+       }
       }
-    }
-    
-}
-
-echo "<pre>";
-$macbook = new macbook ("spacegray","polycarbonaat","vierkant",false,);
-$macbook->accu();
-var_dump($macbook);
-echo "<br>---------<br>";
-
+      $strawberry = new toetsenbord("Macbook Pro 2019","Spacegray","hout","vierkant",false, );
+      $strawberry->intro();
+      $strawberry->message();
